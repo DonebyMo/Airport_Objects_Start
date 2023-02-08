a8c6ffc0236d9630d6dd6428c313700b2f112977
Muhammad <muhammadalnabhani@gmail.com>
2023-02-08 11:47:34 +0000

    passed test for bags.js and person.js

--- Bag.js
+++ Bag.js
@@ -1,8 +1,16 @@
 class Bag {
-    // Include constructor Method with the following property: 
-        // 1. Weight
-            //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+  constructor(weight) {
+    if (!weight) {
+      throw new Error("bag needs weight");
+    }
+
+    this.weight = weight;
+  }
+  // Include constructor Method with the following property:
+  // 1. Weight
+  //1a. Should handle an exception in case user adds no weight and throw an error.
 }
+
+module.exports = Bag;
 
 //Export the module
--- Person.js
+++ Person.js
@@ -1,10 +1,14 @@
 class Person {
-    // Include a Constructor Method for Properties: 
-        // 1. Name
-        // 2. Bags
-            // 2a. Bags should contain/reference a data type for storing more than one bag object.
+  constructor(name, bags) {
+    this.name = name;
+    this.bags = [bags];
+  }
 
-    // Include an addBag method
+  addBag() {
+    console.log("Adding bags...");
+  }
 }
 
 //Export the Module
+
+module.exports = Person;
